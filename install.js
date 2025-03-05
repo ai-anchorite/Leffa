0440d5bac7859c1de7db36de54cb6adbc6e4260a
ai_anchorite <ai_anchorite@proton.me>
2025-03-05 13:21:07 +1300

    Add files via upload

---
+++ install.js
@@ -0,0 +1,52 @@
+module.exports = {
+  run: [
+  
+    {
+     when: "{{gpu !== 'nvidia'}}",
+     method: "notify",
+     params: {
+       html: "This app requires an NVIDIA GPU."
+     }, 
+       next: null
+    },
+    
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/ai-anchorite/Leffa app",
+        ]
+      }
+    },
+    
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",               
+          path: "app",                
+        }
+      }
+    },
+    
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",               
+        path: "app",               
+        message: [
+          "uv pip install -r requirements.txt"
+        ]
+      }
+    },
+    
+    {
+      method: 'input',
+      params: {
+        title: 'Installation complete',
+        description: 'Click "Start" on the left sidebar to run. Note that Leffa will download 20GB of models on first start. If downloads are interrupted, just start it again and it will continue.'
+      }
+    },
+  ]
+}
